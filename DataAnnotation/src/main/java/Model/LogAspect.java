1e8c0b36b89da0720b56d88f7341c22af6fc6705
MoKeqing <841815805@qq.com>
2021-05-16 16:28:08 +0800

    汇总日志类

---
+++ DataAnnotation/src/main/java/Model/LogAspect.java
@@ -0,0 +1,28 @@
+package Model;
+
+
+import org.apache.log4j.Logger;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+
+@Aspect
+@Component
+public class LogAspect {
+	@After("@annotation(logAnnotation)")
+	public void log(JoinPoint joinPoint,LogAnnotation logAnnotation) {
+		String className = logAnnotation.className();
+		String content = logAnnotation.content();
+		try {
+			
+			Class<?> cls = Class.forName(className);
+			Logger log = Logger.getLogger(cls);
+			log.info(content);
+
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+}
