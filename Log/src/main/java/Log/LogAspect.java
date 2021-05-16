077ca8ff8f2f8fd706904adb5e49db1891797952
Tonz <821533669@qq.com>
2021-05-16 14:59:25 +0800

    Log打印日志时不获取对应方法的参数

--- Log/src/main/java/Log/LogAspect.java
+++ Log/src/main/java/Log/LogAspect.java
@@ -16,20 +16,11 @@ public class LogAspect {
 		String className = logAnnotation.className();
 		String content = logAnnotation.content();
 		try {
-			//获取想要打印日志的方法的参数，这里只使用第一个参数，用于爬虫的日志编写
-			Object[] args = joinPoint.getArgs();
 			
-			if(args.length>0) {
-			String id = (String) args[0];
 			Class<?> cls = Class.forName(className);
 			Logger log = Logger.getLogger(cls);
-			log.info(content+id);		
-			}else {
-				Class<?> cls = Class.forName(className);
-				Logger log = Logger.getLogger(cls);
-				log.info(content);
-			}
-			
+			log.info(content);
+
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
